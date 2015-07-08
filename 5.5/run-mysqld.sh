9b7220055e1c2c29938282e9f33b70ae2b3135fa
Michal Fojtik <mfojtik@redhat.com>
2015-07-08 11:49:02 +0200

    Allow to set passwords for non-clustered mysql

--- 5.5/run-mysqld.sh
+++ 5.5/run-mysqld.sh
@@ -22,8 +22,24 @@ if [ "$1" == "mysqld" ]; then
 
   if [ ! -d "$MYSQL_DATADIR/mysql" ]; then
     initialize_database
-    mysqladmin $admin_flags flush-privileges shutdown
+  else
+    start_local_mysql
   fi
+
+# Set the password for MySQL user and root everytime this container is started.
+# This allows to change the password by editing the deployment configuration.
+mysql $mysql_flags <<EOSQL
+  SET PASSWORD FOR '${MYSQL_USER}'@'%' = PASSWORD('${MYSQL_PASSWORD}');
+EOSQL
+
+# The MYSQL_ROOT_PASSWORD is optional
+if [ -v MYSQL_ROOT_PASSWORD ]; then
+mysql $mysql_flags <<EOSQL
+    SET PASSWORD FOR 'root'@'%' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
+EOSQL
+fi
+
+  mysqladmin $admin_flags flush-privileges shutdown
 
   shift
   unset_env_vars
