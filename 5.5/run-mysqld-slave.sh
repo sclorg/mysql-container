156e4d409bc9557e8469b8025b22e206869c1d79
Honza Horak <hhorak@redhat.com>
2015-07-09 23:47:07 +0200

    Get binlog file and position from master rather than having it hardcoded

--- 5.5/run-mysqld-slave.sh
+++ 5.5/run-mysqld-slave.sh
@@ -26,8 +26,18 @@ export MYSQL_DISABLE_CREATE_DB=1
 initialize_database
 wait_for_mysql_master
 
+# Get binlog file and position from master
+STATUS_INFO=$(mysql --host "$(mysql_master_addr)" "-u${MYSQL_MASTER_USER}" "-p${MYSQL_MASTER_PASSWORD}" -e 'SHOW MASTER STATUS\G')
+BINLOG_POSITION=$(echo "$STATUS_INFO" | grep 'Position:' | head -n 1 | sed -e 's/^\s*Position: //')
+BINLOG_FILE=$(echo "$STATUS_INFO" | grep 'File:' | head -n 1 | sed -e 's/^\s*File: //')
+
+if [ -z "${BINLOG_FILE}" -o -z "${BINLOG_POSITION}" ] ; then
+  echo "Could not read binlog position or file from master"
+  exit 1
+fi
+
 mysql $mysql_flags <<EOSQL
-  CHANGE MASTER TO MASTER_HOST='$(mysql_master_addr)',MASTER_USER='${MYSQL_MASTER_USER}', MASTER_PASSWORD='${MYSQL_MASTER_PASSWORD}', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=107;
+  CHANGE MASTER TO MASTER_HOST='$(mysql_master_addr)',MASTER_USER='${MYSQL_MASTER_USER}', MASTER_PASSWORD='${MYSQL_MASTER_PASSWORD}', MASTER_LOG_FILE='${BINLOG_FILE}', MASTER_LOG_POS=${BINLOG_POSITION};
   SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1; SLAVE START;
 EOSQL
 
