7ea10a75763d67a2b44076bb4515148917bf28fa
Honza Horak <hhorak@redhat.com>
2015-08-28 10:21:33 +0200

    Adding quotes to prevent some unneeded variables expansions

--- 5.5/atomic-contrib/uninstall.sh
+++ 5.5/atomic-contrib/uninstall.sh
@@ -2,7 +2,7 @@
 
 . /usr/share/container-layer/mysql/atomic/include.sh
 
-chroot ${HOST} /usr/bin/systemctl disable ${service_name}.service
-chroot ${HOST} /usr/bin/systemctl stop ${service_name}.service
-rm -f ${HOST}/etc/systemd/system/${service_name}.service
+chroot "${HOST}" /usr/bin/systemctl disable "${service_name}.service"
+chroot "${HOST}" /usr/bin/systemctl stop "${service_name}.service"
+rm -f "${HOST}/etc/systemd/system/${service_name}.service"
 
