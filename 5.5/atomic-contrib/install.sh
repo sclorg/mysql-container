1a7f7ef375972bf192dd287474b24b3dc411ad74
Honza Horak <hhorak@redhat.com>
2015-09-10 15:55:34 +0200

    We don't need to call chcon in case we mount with :Z

--- 5.5/atomic-contrib/install.sh
+++ 5.5/atomic-contrib/install.sh
@@ -5,7 +5,6 @@ MYSQL_GID=27
 
 if ! [ -d "${DATADIR}" ] ; then
   mkdir -p "${DATADIR}"
-  chcon -Rt svirt_sandbox_file_t "${DATADIR}"
   chown -R ${MYSQL_UID}.${MYSQL_GID} "${DATADIR}"
   chmod -R 770 "${DATADIR}"
 fi
