d0bc983e7be06b3c4249022933e979bbbc00391d
Petr "Stone" Hracek <phracek@redhat.com>
2019-10-01 09:46:50 +0200

    Fixes BZ#1653215 Operation ALTER USER failed
    This commit fixes problem formatting MySQL commands
    in 50-passwd-change.sh script.
    Correct BASH syntax as an input to MySQL command is used.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- root-common/usr/share/container-scripts/mysql/init/50-passwd-change.sh
+++ root-common/usr/share/container-scripts/mysql/init/50-passwd-change.sh
@@ -4,9 +4,22 @@ password_change() {
   # Set the password for MySQL user and root everytime this container is started.
   # This allows to change the password by editing the deployment configuration.
   if [[ -v MYSQL_USER && -v MYSQL_PASSWORD ]]; then
-mysql $mysql_flags <<EOSQL
-      ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    TMP_FILE=$(mktemp /tmp/mysql_userXXXXX)
+mysql $mysql_flags >> $TMP_FILE <<EOSQL
+      SELECT user FROM mysql.user;
 EOSQL
+    grep -Fxq "$MYSQL_USER" "$TMP_FILE" > /dev/null
+    RETCODE=$?
+    if [[ $RETCODE -eq 0 ]]; then
+  mysql $mysql_flags <<EOSQL
+        ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+EOSQL
+    else
+      log_info "User ${MYSQL_USER} does not exist in database."
+    fi
+    rm "${TMP_FILE}"
+    unset TMP_FILE
+
   fi
 
   # The MYSQL_ROOT_PASSWORD is optional, therefore we need to either enable remote
