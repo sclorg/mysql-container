c1706c6d5aaf30066662a7b15ce0ac824d1d80cb
Petr "Stone" Hracek <phracek@redhat.com>
2019-10-07 15:09:04 +0200

    Check if user does not exist in variable from mysql output.
    Do not use `grep` command which fails.
    mysql command stores output into /var/lib/mysql/data directory
    which is shared outside of container.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- root-common/usr/share/container-scripts/mysql/init/50-passwd-change.sh
+++ root-common/usr/share/container-scripts/mysql/init/50-passwd-change.sh
@@ -4,21 +4,16 @@ password_change() {
   # Set the password for MySQL user and root everytime this container is started.
   # This allows to change the password by editing the deployment configuration.
   if [[ -v MYSQL_USER && -v MYSQL_PASSWORD ]]; then
-    TMP_FILE=$(mktemp /tmp/mysql_userXXXXX)
-mysql $mysql_flags >> $TMP_FILE <<EOSQL
-      SELECT user FROM mysql.user;
-EOSQL
-    grep -Fxq "$MYSQL_USER" "$TMP_FILE" > /dev/null
-    RETCODE=$?
-    if [[ $RETCODE -eq 0 ]]; then
+    USERS=$(echo "SELECT user FROM mysql.user;" | mysql $mysql_flags)
+    USERS=$(echo "${USERS}" | tr '\n' ' ')
+    if [[ "$USERS" == *"${MYSQL_USER}"* ]]; then
   mysql $mysql_flags <<EOSQL
         ALTER USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 EOSQL
     else
       log_info "User ${MYSQL_USER} does not exist in database."
     fi
-    rm "${TMP_FILE}"
-    unset TMP_FILE
+    unset USERS
 
   fi
 
